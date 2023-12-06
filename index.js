28fbce89294aec6335411f23fb134c470c983d52
Denis <str.comparetoignore@gmail.com>
2023-12-06 23:34:28 +0700

    add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {Server} from 'socket.io'
 import cors from 'cors'
 
 
+const PORT = process.env.PORT||3000
 const app = express().use(cors)
 app.use(cors())
 const server = http.createServer(app)
@@ -21,7 +22,7 @@ io.on("connection", async (socket)=>{
 })
 
 
-server.listen(3000, ()=>{
+server.listen(PORT, ()=>{
     console.log("server is running on port 3000");
 })
 
